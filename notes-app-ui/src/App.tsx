1826e4e9eb4f35d3e5d4917b7bae0cab8e2812a1
Konrad <konrad.p.dudek@gmail.com>
2024-03-13 22:31:05 +0100

    added form action, submitting new form adds a note to static list

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import {useState} from "react";
+import React, {useState} from "react";
 
 type Note = {
     id: number
@@ -38,17 +38,60 @@ const App = () => {
             content: "content5"
         }
     ])
+
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+
+    const handleSubmit = (
+        event: React.FormEvent
+    ) => {
+        event.preventDefault()
+        console.log("title: ", title)
+        console.log("content: ", content)
+
+        //we create new note when someone clicks submit
+        const newNote: Note = {
+            id: notes.length + 1,
+            title: title,
+            content: content
+        }
+
+        //we call setNotes function, passing new note and existing notes
+        setNotes([newNote, ...notes])
+        //we clear title and content so after submit form gets cleared
+        setTitle("")
+        setContent("")
+
+    }
+
     return(
         <div className="app-container">
-            <form className="note-form">
-                <input placeholder="title" required>
+
+            {/*form*/}
+            <form
+                className="note-form"
+                onSubmit={(event) => handleSubmit(event)}>
+                <input
+                    value={title}
+                    onChange={(event) =>
+                        setTitle(event.target.value)}
+                    placeholder="title"
+                    required>
                 </input>
-                <textarea placeholder="title" rows={10} required>
+                <textarea
+                    value={content}
+                    onChange={(event)=>
+                        setContent(event.target.value)}
+                    placeholder="title"
+                    rows={10}
+                    required>
                 </textarea>
                 <button type="submit">
                     Add Note
                 </button>
             </form>
+
+            {/*all notes*/}
             <div className="notes-grid">
                 {notes.map((note) => (
                     <div className="note-item">
